173b28eadb9b89636217b0379150999548348b13
Ankit Singh <31512629+AnkitAvi1311@users.noreply.github.com>
2018-01-27 18:39:23 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var menuref=document.getElementById("ham");
-menuref.addEventListener("click",function(){
-    document.getElementById("ham-content").style.width="75%";
-});
-
-document.getElementById("close").addEventListener("click",function(){
-    document.getElementById("ham-content").style.width="0";
-});
