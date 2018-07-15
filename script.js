63a36ca33d83e1bfd712992767dde5fab0fed56d
Ankit Singh <31512629+AnkitAvi1311@users.noreply.github.com>
2018-07-15 18:42:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-"use restrict";
-document.getElementById("drop").addEventListener("click",function(){
-    document.getElementById("account").classList.toggle("foraccount");
-    document.getElementsByClassName("fa-sort-down")[0].classList.toggle("rotate");
-});
+"use strict"
 
-document.getElementById("menu").addEventListener("click",function(){
-    document.getElementsByClassName("nav-content")[0].classList.add("fornav");
-    document.getElementById("body-over").classList.add("forbody");
-});
-
-document.getElementById("body-over").addEventListener("click",function(){
-    document.getElementsByClassName("nav-content")[0].classList.remove("fornav");
-    document.getElementById("body-over").classList.remove("forbody");
-});
