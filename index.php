387295aad38d56f8d5cfadb328694753011f558c
Ankit Singh <31512629+AnkitAvi1311@users.noreply.github.com>
2018-09-15 12:37:12 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,41 +1,16 @@
-<!DOCTYPE html>
-<html lang="eng-us">
-    <head>
-        <title>Home</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        
-        <form method="POST">
-            <input type="text" name="person">
-            <button>SUBMIT</button>
-        </form>
-        
-        <?php
-            
-            // demonstration of variables in PHP
-            $name = $_POST["person"];
-            if($name){
-                echo $name." is a handsome guy";
-            }
-        
-            //this is a single line comment 
-        
-            /*
-                This is a multi
-                line comment 
-                This gets something from the database
-            */
-        
-            //this is a demonstration of user defined function
-            function myfunction(){
-                echo "This is a function";
-            }
-            //calling or invoking the function
-            myFunction();                        
-        
-        ?>
-        
-    </body>
-</html>
+<?php
+//file required for the connection to database class
+include "connect/connect.php";
+
+$db = new DatabaseConnect("localhost","root","");
+$conn = $db->connect();
+
+$db->createDatabase($conn, "myworld2");
+
+// ERROR HANDLING IN PHP
+function customError($no, $erstr) {
+    echo "<br/>Error : ".$no ."  ".$erstr;
+    die();
+}
+set_error_handler("customError");
+echo $helloworld;
